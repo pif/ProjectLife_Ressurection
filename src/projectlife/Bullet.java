e5b240a3369c1b54c1d671e3dab03a35935a9f01
Ostap <pifostap@gmail.com>
2010-05-02 00:43:17 +0300

    bulllets' kill() should return an array. weapon should define the action...or not... cause action is usually: health decrease

--- src/projectlife/Bullet.java
+++ src/projectlife/Bullet.java
@@ -22,6 +22,8 @@ public class Bullet extends MovingObject {
 		}
 	}
 
+	//draw the bullet...
+	//use Animation class!
 	public boolean display() {
 		for (int i = 0; i < bulletsTrail.length; i++) {
 			p.fill(100 + i * 3);
@@ -34,8 +36,12 @@ public class Bullet extends MovingObject {
 		return super.display();
 	}
 
-	//hmm... that seems to me that every weapon should have it's own bullet class..
-	//maybe, we don't even need different weapons...we just need different bullets???
+	// hmm... that seems to me that every weapon should have it's own bullet
+	// class..
+	// maybe, we don't even need different weapons...we just need different
+	// bullets???
+	// that's the method which moves a bullet. for different weapons this should
+	// be quite different
 	public void move() {
 		// acceleration.normalize();
 		if (!stopped) {
@@ -54,9 +60,12 @@ public class Bullet extends MovingObject {
 			bulletsTrail[bulletsTrail.length - 1] = location;
 		}
 	}
-
+	
+	//needless array... Animation owned u
 	public PVector[] bulletsTrail;
 
+	// should return an object, which was hit...and weapon should think WHAT to do with theM!!!! 
+	//maybe i should return an array of IHittable objects?
 	public int kill() {
 		for (int i = 0; i < p.level.beasts.length; i++) {
 			if (PApplet.dist(this.location.x, this.location.y,
